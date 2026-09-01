4fb67a6950c0761d110c30adbd87783f2e6fae11
Kumar Ayush <kumar.ayush@outlook.com>
2026-09-01 09:20:47 +0530

    Implemented doctor apis and fixed patient apis

--- doctors/views.py
+++ doctors/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
 
+from rest_framework.views import APIView,Response
+from .models import Doctor
+from .serializers import DoctorSerializer
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 # Create your views here.
+class DoctorManagement(APIView):
+    def get(self,request,id=None):
+        if id==None:
+            doctors=Doctor.objects.all()
+            doctorsData=DoctorSerializer(doctors,many=True)
+            return Response(doctorsData.data,status=status.HTTP_200_OK)
+        doctor=get_object_or_404(Doctor,id=id)
+        doctorData=DoctorSerializer(doctor)
+        return Response(doctorData.data,status=status.HTTP_200_OK)
+
+    def post(self,request):
+        doctorData=DoctorSerializer(data=request.data)
+        if doctorData.is_valid():
+            doctor=doctorData.save()
+            return Response(DoctorSerializer(doctor).data,status=status.HTTP_201_CREATED)
+        return Response(doctorData.errors,status=status.HTTP_400_BAD_REQUEST)      
+    
+    def put(self,request,id):
+        doctor=get_object_or_404(Doctor,id=id)
+        doctorData=DoctorSerializer(doctor,data=request.data)
+        if doctorData.is_valid():
+            doctor=doctorData.save()
+            return Response(DoctorSerializer(doctor).data,status=status.HTTP_200_OK)
+        return Response(doctorData.errors,status=status.HTTP_400_BAD_REQUEST)   
+    def patch(self,request,id):
+        doctor=get_object_or_404(Doctor,id=id)
+        doctorData=DoctorSerializer(doctor,data=request.data,partial=True)
+        if doctorData.is_valid():
+            doctor=doctorData.save()
+            return Response(DoctorSerializer(doctor).data,status=status.HTTP_200_OK)
+        return Response(doctorData.errors,status=status.HTTP_400_BAD_REQUEST)   
+    
+    def delete(self,request,id):
+        doctor=get_object_or_404(Doctor,id=id)
+        doctor.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+        
--- patients/views.py
+++ patients/views.py
@@ -1,8 +1,9 @@
-from patients import serializers
+
 from rest_framework.views import APIView,Response
 from .models import Patient
 from .serializers import PatientSerializer
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 # Create your views here.
 class PatientManagement(APIView):
     def get(self,request,id=None):
@@ -10,7 +11,7 @@ class PatientManagement(APIView):
             patients=Patient.objects.all()
             patientsData=PatientSerializer(patients,many=True)
             return Response(patientsData.data,status=status.HTTP_200_OK)
-        patient=Patient.objects.get(id=id)
+        patient=get_object_or_404(Patient,id=id)
         patientData=PatientSerializer(patient)
         return Response(patientData.data,status=status.HTTP_200_OK)
 
@@ -24,18 +25,27 @@ class PatientManagement(APIView):
                 status=status.HTTP_201_CREATED
             )
         return Response(
+                patientData.errors,
                 status=status.HTTP_400_BAD_REQUEST
         )  
     
-    def delete(self,request,id):
-        patient=Patient.objects.get(id=id)
-        patient.delete()
-        return Response({"message":"Delete Patient"})
-    
     def put(self,request,id):
-        patient=Patient.objects.get(id=id)
+        patient=get_object_or_404(Patient,id=id)
         patientData=PatientSerializer(patient,data=request.data)
         if patientData.is_valid():
             patient=patientData.save()
             return Response(PatientSerializer(patient).data,status=status.HTTP_200_OK)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(patientData.errors,status=status.HTTP_400_BAD_REQUEST)
+    def patch(self,request,id):
+        patient=get_object_or_404(Patient,id=id)
+        patientData=PatientSerializer(patient,data=request.data,partial=True)
+        if patientData.is_valid():
+            patient=patientData.save()
+            return Response(PatientSerializer(patient).data,status=status.HTTP_200_OK)
+        return Response(patientData.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,id):
+        patient=get_object_or_404(Patient,id=id)
+        patient.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
